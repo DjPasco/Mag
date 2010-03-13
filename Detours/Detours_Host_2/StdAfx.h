e60ec44257e34a2fef5d5246b79826f579e74d84
Dainius <vgtu.dainius@gmail.com>
2010-03-13 14:42:57 +0200

    Deleted DetourHost and Detour_Host_2

--- Detours/Detours_Host_2/StdAfx.h
+++
@@ -1,9 +0,0 @@
-#if !defined(AFX_STDAFX_H__E1AAA9C1_E812_417E_968E_35A98E25E3AC__INCLUDED_)
-#define AFX_STDAFX_H__E1AAA9C1_E812_417E_968E_35A98E25E3AC__INCLUDED_
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-
-#include <stdio.h>
-
-#endif // !defined(AFX_STDAFX_H__E1AAA9C1_E812_417E_968E_35A98E25E3AC__INCLUDED_)
